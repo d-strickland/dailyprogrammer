98c9bb63c674421d03765e3042969ce3294f872b
David Strickland <dstrick2989@gmail.com>
2015-02-23 18:54:20 -0600

    Solved bank numbers.

---
+++ haskell/src/DailyProgrammer/BankNumbers.hs
@@ -0,0 +1,61 @@
+module DailyProgrammer.BankNumbers where
+import Data.List (transpose, foldl', elemIndex)
+import Data.Char (digitToInt, intToDigit)
+
+main = do
+    putStrLn . banner $ "0123456789"
+    putStrLn . showFromBanner . fromBanner . banner $ "0123456789"
+
+banner :: String -> String
+banner = unlines . transpose . bannerT
+    where bannerT = foldl' acc []
+          xs `acc` x = xs ++ (transpose $ nums!!(digitToInt x))
+
+fromBanner :: String -> Maybe [Int]
+fromBanner = sequence . fromBannerT . transpose . lines
+fromBannerT [] = []
+fromBannerT (x:y:z:xs) = (elemIndex (transpose $ [x,y,z]) nums) : (fromBannerT xs)
+
+showFromBanner Nothing = "Bad banner."
+showFromBanner (Just ns) = map intToDigit ns
+
+nums = [[" _ ",
+         "| |",
+         "|_|"],
+
+        ["   ",
+         "  |",
+         "  |"],
+
+        [" _ ",
+         " _|",
+         "|_ "],
+
+        [" _ ",
+         " _|",
+         " _|"],
+
+        ["   ",
+         "|_|",
+         "  |"],
+
+        [" _ ",
+         "|_ ",
+         " _|"],
+
+        [" _ ",
+         "|_ ",
+         "|_|"],
+
+        [" _ ",
+         "  |",
+         "  |"],
+
+        [" _ ",
+         "|_|",
+         "|_|"],
+
+        [" _ ",
+         "|_|",
+         " _|"]]
+
--- haskell/src/DailyProgrammer/RailFence.hs
+++ haskell/src/DailyProgrammer/RailFence.hs
@@ -7,6 +7,9 @@ main = do
     putStrLn $ enc 3 "REDDITCOMRDAILYPROGRAMMER"
     putStrLn $ dec 3 "RIMIRAREDTORALPORMEDCDYGM"
 
+    putStrLn $ enc 2 "LOLOLOLOLOLOLOLOLOLOLOL"
+    putStrLn $ dec 2 "LLLLLLLLLLLLOOOOOOOOOOO"
+
 zigZag n = cycle $ [1..n] ++ [n-1..2]
 xs `sortedBy` ys = map fst (sortBy (comparing snd) $ zip xs ys)
 enc n message = message `sortedBy` zigZag n
