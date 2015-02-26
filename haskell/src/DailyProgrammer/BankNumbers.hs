fcf84738033501416c83ffbfd9030e6cf3e1f723
David Strickland <dstrick2989@gmail.com>
2015-02-26 11:43:12 -0600

    Added some function types for clarity.

--- haskell/src/DailyProgrammer/BankNumbers.hs
+++ haskell/src/DailyProgrammer/BankNumbers.hs
@@ -7,11 +7,15 @@ main = do putStrLn . banner $ "0123456789"
 
 banner :: String -> String
 banner = unlines . transpose . bannerT
+
+bannerT :: String -> [String]
 bannerT = foldl' acc []
     where acc xs x = xs ++ (transpose $ nums!!(digitToInt x))
 
 fromBanner :: String -> Maybe [Int]
 fromBanner = sequence . fromBannerT . transpose . lines
+
+fromBannerT :: [String] -> [Maybe Int]
 fromBannerT [] = []
 fromBannerT (x:y:z:xs) = (elemIndex (transpose [x,y,z]) nums) : (fromBannerT xs)
 
