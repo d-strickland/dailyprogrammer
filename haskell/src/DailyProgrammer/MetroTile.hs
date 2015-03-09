71618b8a58e1f2d0f38780bebed7800977542403
David Strickland <dstrick2989@gmail.com>
2015-03-09 18:06:20 -0500

    Cleaned up MetroTile for presentation.

--- haskell/src/DailyProgrammer/MetroTile.hs
+++ haskell/src/DailyProgrammer/MetroTile.hs
@@ -2,14 +2,14 @@ module DailyProgrammer.MetroTile where
 
 import Data.Monoid (mappend)
 import Data.List (foldl')
-import qualified Data.Foldable as F
 import qualified Data.Set as S
 
 main = do
     bounds <- getLine
     chars <- getContents
     let [xb, yb] = map read $ words bounds
-    F.mapM_ print . foldl' addPixel S.empty . pixels xb yb . concat . lines $ chars
+        pixels   = toPixels xb yb . concat . lines $ chars
+    mapM_ print . S.toAscList . foldl' addPixel S.empty $ pixels
 
 data Window = Window { title  :: Char
                      , xpos   :: Int
@@ -37,31 +37,42 @@ instance Show Window where
 
 {- |Take an x-bound, y-bound, and a list of characters and generate a list of
     "pixels" represented by 1x1 windows. -}
-pixels :: Int -> Int -> [Char] -> [Window]
-pixels xb yb chars = map mkPix . filter ((/='.') . first) $ zipWith triple chars positions
-    where positions      = [(x,y) | y <- [0..yb-1], x <- [0..xb-1]] -- [(0,0),...,(xb-1,yb-1)]
-          triple a (b,c) = (a,b,c)
-          first (x,_,_)  = x
-          mkPix (c,x,y)  = Window {title=c, xpos=x, ypos=y, width=1, height=1}
+toPixels :: Int -> Int -> [Char] -> [Window]
+toPixels xb yb chars = map mkPix . filter ((/='.') . char) $ zip3 chars xs ys
+    where xs = cycle [0..xb-1]
+          ys = concat [take xb $ repeat y | y <- [0..yb-1]]
+          char (c,_,_) = c
+          mkPix (c,x,y) = Window {title=c, xpos=x, ypos=y, width=1, height=1}
 
 {- |Accumulation function for the Set of windows. Take a Set of Windows
     and a pixel. Add the pixel to the Set by either expanding one of the
-    windows or adding a new window. Set is implemented with balanced binary
-    search trees, so that should keep the windows ordered. O(size wins) -}
+    windows or adding a new window. Set is implemented as a balanced binary
+    search tree, so that should keep the windows ordered and keep insertion
+    efficient. O(size wins) -}
 addPixel :: S.Set Window -> Window -> S.Set Window
-wins `addPixel` p | S.null adjWins = p `S.insert` wins
-                  | otherwise      = bigWin `S.insert` wins
-                  where adjWins = S.filter (`contains` p) wins
-                        bigWin  = (head $ S.toList adjWins) `merge` p
+wins `addPixel` p | not $ S.null contWins = wins
+                  | not $ S.null adjWins  = bigWin `S.insert` wins
+                  | otherwise             = p `S.insert` wins
+                  where contWins = S.filter (`contains` p) wins
+                        adjWins  = S.filter (`adjoins` p) wins
+                        bigWin   = (head $ S.toList adjWins) `merge` p
 
 Window t1 x1 y1 w1 h1 `contains` Window t2 x2 y2 _ _
     | t1 /= t2 = False
     | x2 < x1  = False
     | y2 < y1  = False
-    | (x2 < x1 + w1) && (y2 <= y1 + h1) = True
-    | (y2 < y1 + h1) && (x2 <= x1 + w1) = True
+    | (x2 < x1 + w1) && (y2 < y1 + h1) = True
+    | (y2 < y1 + h1) && (x2 < x1 + w1) = True
     | otherwise = False
 
-Window t1 x1 y1 w1 h1 `merge` Window t2 x2 y2 w2 h2
+Window t1 x1 y1 w1 h1 `adjoins` Window t2 x2 y2 _ _
+    | t1 /= t2 = False
+    | x2 < x1  = False
+    | y2 < y1  = False
+    | (x2 < x1 + w1) && (y2 == y1 + h1) = True
+    | (y2 < y1 + h1) && (x2 == x1 + w1) = True
+    | otherwise = False
+
+Window t1 x1 y1 w1 h1 `merge` Window _ x2 y2 _ _
     = Window t1 x1 y1 (max w1 (x2-x1+1)) (max h1 (y2-y1+1))
 
