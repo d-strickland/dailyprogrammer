91e3b9f187cf4dad9d298514391a3fa5b7a82415
David Strickland <dstrick2989@gmail.com>
2015-06-10 22:58:17 -0500

    Added next animal question logic.

--- python/animals.py
+++ python/animals.py
@@ -6,14 +6,14 @@ ANIMAL_FILE = 'data/animals.pkl'
 
 questions = dict()
 animals = set()
-remaining = set()
+candidates = set()
 
 def init():
     with open(QUESTION_FILE, 'rb') as qf, open(ANIMAL_FILE, 'rb') as af:
         questions = pickle.load(qf)
         animals = pickle.load(af)
 
-    remaining = set(animals)  # Start out with the full set and narrow things down.
+    candidates = set(animals)  # Start out with the full set and narrow things down.
 
     print('Welcome to Animal Guess. Please think of an animal.')
     input('Press Enter to continue.\n')
@@ -22,8 +22,17 @@ def init():
 def main():
     init()
     
-def get_question():
-    pass
+def polarization(q):
+    """An index of how well a question partitions the set of candidate animals.
+
+    A lower value represents a better partition. The perfect partition splits the
+    candidate set exactly in half, so target that.
+    """
+    overlap = questions[q].intersection(candidates)
+    return abs(len(overlap) - (len(candidates) / 2))
+
+def next_question():
+    return min(questions.keys(), key=polarization)
 
 def yesorno(prompt):
     response = input(prompt)
