a00a41a3c0d38a5ccc3631a5f98e09a527519054
David Strickland <dstrick2989@gmail.com>
2015-09-16 22:46:36 -0500

    Fixed pi estimate problem for circles with no white pixels on the side.

--- haskell/src/DailyProgrammer/EstimatePI.hs
+++ haskell/src/DailyProgrammer/EstimatePI.hs
@@ -2,20 +2,24 @@ module DailyProgrammer.EstimatePI where
 -- https://www.reddit.com/r/dailyprogrammer/comments/3f0hzk/20150729_challenge_225_intermediate_estimating_pi/
 
 import Data.Either
-import Data.List.Split (splitOn)
+import Data.List.Split (chunksOf)
 import System.Environment (getArgs)
 import Vision.Image
 import Vision.Image.Storage.DevIL
+import Vision.Primitive.Shape (Z (..), (:.) (..))
 import qualified Data.Vector.Storable as V
 
 black = RGBPixel 0 0 0
-white = RGBPixel 255 255 255
+
+height ((Z :. h) :. _) = h
 
 area :: RGB -> Int
 area = V.length . V.filter (==black) . manifestVector
 
 diameter :: RGB -> Int
-diameter = maximum . Prelude.map length . splitOn [white] . V.toList . manifestVector
+diameter img = length . filter (black `elem`) . chunksOf h . V.toList
+                        . manifestVector $ img
+    where h = height . manifestSize $ img
 
 piEst :: RGB -> Double
 piEst circle = a / (r*r)
